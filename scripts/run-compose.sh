e99cb37ca9efcc05b9797fbf332248a63abdc568
kaotu <kao-tu@hotmail.co.th>
2020-09-12 10:35:54 +0700

    💚 Update docker-compose file name 🌚

--- scripts/run-compose.sh
+++ scripts/run-compose.sh
@@ -3,5 +3,5 @@
 echo Loggin AWS ECR
     $(aws ecr get-login --region ap-southeast-2 --no-include-email)
 
-docker-compose -f /home/centos/docker-compose.deploy.yml pull
-docker-compose -f /home/centos/docker-compose.deploy.yml up -d
+docker-compose -f /home/centos/docker-compose-demo.deploy.yml pull
+docker-compose -f /home/centos/docker-compose-demo.deploy.yml up -V -d
